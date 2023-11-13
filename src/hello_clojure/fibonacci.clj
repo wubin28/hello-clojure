883e3bb928e830153cf5fca7a59355317e7df8cf
Ben Wu <wubin28@gmail.com>
2023-11-13 19:56:59 +0800

    feat: add fibonacci code from chapter 3 of the book Functional Design

---
+++ src/hello_clojure/fibonacci.clj
@@ -0,0 +1,12 @@
+(defn fibs-work [n i fs]
+  (if (= i n)
+    fs
+    (fibs-work n (inc i) (conj fs (apply + (take-last 2 fs))))))
+
+(defn fibs [n]
+  (cond
+    (< n 1) []
+    (= n 1) [1]
+    :else (fibs-work n 2 [1 1])))
+
+(fibs 15)
